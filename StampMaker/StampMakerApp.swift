6309c7fb57aa5df8e842fcff3393dcc400930565
tsuyoshi kurata <tsuyoshi_kurata@shimapri.jp>
2023-02-03 13:20:32 +0900

    Initial Commit

---
+++ StampMaker/StampMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StampMakerApp.swift
+//  StampMaker
+//
+//  Created by 蔵田 剛 on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct StampMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
